e0490dbed250cd04c8a081b4ca60ba61c65d8dec
cem-sucu <cems.cems.17@gmail.com>
2022-08-06 21:36:33 +0200

    logo navbar +css

--- src/Menu.js
+++ src/Menu.js
@@ -1,25 +1,36 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import logo from "./One-Piece-logo-remove.png";
 
 function Menu() {
     return (
         <nav className="navbar">
             <ul className="navul">
                 <li className="navli">
-                    <a className="nava" href="/">Accueil</a>
+                    <img className="navlogo" src={logo} alt="logo" />
                 </li>
                 <li className="navli">
-                    <a className="nava" href="/">Personnage</a>
+                    <a className="nava" href="/">
+                        Accueil
+                    </a>
                 </li>
                 <li className="navli">
-                    <a className="nava" href="/">Ennemi</a>
+                    <a className="nava" href="/">
+                        Personnage
+                    </a>
                 </li>
                 <li className="navli">
-                    <a className="nava" href="/">Fruits du démon</a>
+                    <a className="nava" href="/">
+                        Ennemi
+                    </a>
+                </li>
+                <li className="navli">
+                    <a className="nava" href="/">
+                        Fruits du démon
+                    </a>
                 </li>
             </ul>
         </nav>
-    )
-        
-    }
+    );
+}
 export default Menu;
