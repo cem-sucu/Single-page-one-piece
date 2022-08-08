250a2545530552c7eb6ccd69bf9afa120f6cf12e
cem-sucu <cems.cems.17@gmail.com>
2022-08-08 23:23:34 +0200

    mise en place acceuil + image et style animation css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import WallPaper from "../assets/images/homeWallPaper.jpg";
+// import WallPaper from "../assets/images/homeWallPaper.jpg";
+import ocean from "../assets/images/ocean.png";
 import "../assets/styles/home.css";
 
 const Home = () => {
     return (
-        <div className="container">
+        /*   <div className="container">
             <img className="wallpaper" src={WallPaper} alt="wallpaper" />
+        </div> */
+
+        <div className="container">
+            <h1 aria-label="OnePiece"> </h1>
+            <img className="wallpaper" src={ocean} alt="wallpaper" />
         </div>
     );
 };
