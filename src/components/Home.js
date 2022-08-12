86586dfad8fdd48d30d584c3fddc95c14e71f2e1
cem-sucu <cems.cems.17@gmail.com>
2022-08-12 09:51:23 +0200

    implémentaion effet sonores des vagues

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,45 @@
 import React from "react";
 // import WallPaper from "../assets/images/homeWallPaper.jpg";
 import ocean from "../assets/images/ocean.png";
+import waves from "../assets/music/ocean-waves.mp3";
 import "../assets/styles/home.css";
 
-const Home = () => {
-    return (
-        /*   <div className="container">
+class Home extends React.Component {
+    //j'ai transfomer le component en class pour utiliser le state
+    state = {
+        audio: new Audio(waves),
+        isPlaying: false,
+    };
+    //
+    playPause = () => {
+        let isPlaying = this.state.isPlaying;
+
+        if (isPlaying) {
+            this.state.audio.pause();
+        } else {
+            this.state.audio.play();
+        }
+
+        this.setState({ isPlaying: !isPlaying });
+    };
+    //effets sonores vagues
+    wavesBtn = () => {
+        new Audio(waves).play();
+    };
+    render() {
+        return (
+            /*   <div className="container">
             <img className="wallpaper" src={WallPaper} alt="wallpaper" />
         </div> */
 
-        <div className="container-home">
-            <h1 aria-label="OnePiece"> </h1>
-            <img className="wallpaper" src={ocean} alt="wallpaper" />
-        </div>
-    );
-};
+            <div className="container-home">
+                <h1 aria-label="OnePiece" onClick={this.playPause}>
+                    {" "}
+                </h1>
+                <img className="wallpaper" src={ocean} alt="wallpaper" />
+            </div>
+        );
+    }
+}
 
 export default Home;
