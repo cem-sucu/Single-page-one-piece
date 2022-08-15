d2bd873adfd41d3308102ff3eea0e0bfffacae90
cem-sucu <cems.cems.17@gmail.com>
2022-08-15 18:34:47 +0200

    création du premier content gomu utilisation de la data

--- src/components/DevilFruits.js
+++ src/components/DevilFruits.js
@@ -1,9 +1,20 @@
 import React from "react";
+import gomu from "../assets/images/devilFruits/Gomu.jpg";
+import devilFruitsData from "../data/devilFruitsData";
 
-const DevilFruits = () => {
+const DevilFruits = (props) => {
     return (
-        <div className="container">
-            <h2>Fruits du démon </h2>
+        <div className="container-personnage">
+            <div className="contentpersonnage">
+                <h2 className="persotitile">
+                    {devilFruitsData[0].name}
+                </h2>
+                <img src={gomu} alt="gomu" className="imgperso" />
+                <p>{devilFruitsData[0].signification}</p>
+                <p>Utilisateur : {devilFruitsData[0].user}</p>
+                <p>{devilFruitsData[0].utilities}</p>
+                <p>Type : {devilFruitsData[0].type}</p>
+            </div>
         </div>
     );
 };
